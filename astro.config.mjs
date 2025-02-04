e39abc56976b84d42b451ee21d8a0dffcf77ae96
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-02-04 15:27:36 +0000

    Copy OpenAPI route components from starlight-openapi package and use them in the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,9 @@ export default defineConfig({
 								schema: './aptos-spec.json',
 								sidebarMethodBadges: true,
 							},
-						]),
+						], {
+                            routeEntrypoint: './src/components/OpenAPI/Route.astro'
+                        }),
 					],
           sidebar: [
               {
