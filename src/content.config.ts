bc5c04d79ca9482c49e6ea9a03ebf2f83e6061bf
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-02-04 18:37:08 +0000

    Fix 'pnpm check' errors

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,5 +3,7 @@ import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
+  // TODO: Find the root of errors later
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 };
