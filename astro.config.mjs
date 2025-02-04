bee7508da141a4254b3adad72e3efe2936f5340c
Joe <98909677+moonclavedev@users.noreply.github.com>
2025-02-04 08:06:21 -0800

    Enable auto-generation of sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://aptos.dev',
   integrations: [
       starlight({
           title: 'Aptos Docs',
