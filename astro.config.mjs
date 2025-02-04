9f07cf38b3512cb8483f5781341f3a2051135a4b
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-02-04 18:14:05 +0000

    Reformat all code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,52 +1,55 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwindcss from '@tailwindcss/vite';
-import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwindcss from "@tailwindcss/vite";
+import starlightOpenAPI, { openAPISidebarGroups } from "starlight-openapi";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      starlight({
-          title: 'Aptos Docs',
-          social: {
-              github: 'https://github.com/aptos-labs/',
-          },
-					components: {
-						// Override the default `Head` component.
-						PageFrame: './src/components/PageFrame.astro',
-					},
-          plugins: [
-      			// Generate the OpenAPI documentation pages.
-						starlightOpenAPI([
-							{
-								base: 'api',
-								label: 'API',
-								schema: './aptos-spec.json',
-								sidebarMethodBadges: true,
-							},
-						], {
-                            routeEntrypoint: './src/components/OpenAPI/Route.astro'
-                        }),
-					],
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
-              ...openAPISidebarGroups,
+    starlight({
+      title: "Aptos Docs",
+      social: {
+        github: "https://github.com/aptos-labs/",
+      },
+      components: {
+        // Override the default `Head` component.
+        PageFrame: "./src/components/PageFrame.astro",
+      },
+      plugins: [
+        // Generate the OpenAPI documentation pages.
+        starlightOpenAPI(
+          [
+            {
+              base: "api",
+              label: "API",
+              schema: "./aptos-spec.json",
+              sidebarMethodBadges: true,
+            },
           ],
-          customCss: ['./src/globals.css'],
-      })
+          {
+            routeEntrypoint: "./src/components/OpenAPI/Route.astro",
+          },
+        ),
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        ...openAPISidebarGroups,
+      ],
+      customCss: ["./src/globals.css"],
+    }),
   ],
   adapter: vercel(),
   vite: {
