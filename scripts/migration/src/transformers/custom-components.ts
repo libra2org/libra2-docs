e451679125b42ba54e2fd587a04a350fe3ee3a3d
Joe <98909677+moonclavedev@users.noreply.github.com>
2025-02-12 12:40:38 -0800

    [migration] Add YouTube to custom components transformation

--- scripts/migration/src/transformers/custom-components.ts
+++ scripts/migration/src/transformers/custom-components.ts
@@ -16,6 +16,7 @@ export class CustomComponentTransformer extends BaseTransformer {
     "DynamicApiReference",
     "fetchApiReference",
     "Faucet",
+    "YouTube",
   ];
   protected oldImportPath = "@components/index";
   protected newImportPath = "";
