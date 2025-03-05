23c8230cf3754cc2eeda49d833bf49c84b5da27d
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-03-05 12:42:35 +0000

    Assert that JSX generated by satori-html is compatible with ReactElement

--- src/lib/og-image/generateImage.ts
+++ src/lib/og-image/generateImage.ts
@@ -1,10 +1,11 @@
 import { ImageResponse } from "@vercel/og";
+import type { ReactElement } from "react";
 import { html } from "satori-html";
 
 export function generateImage(options: ConstructorParameters<typeof ImageResponse>[1]) {
   return function (template: string | TemplateStringsArray, ...expressions: unknown[]) {
     const markup = html(template, ...expressions);
 
-    return new ImageResponse(markup, options);
+    return new ImageResponse(markup as ReactElement, options);
   };
 }
