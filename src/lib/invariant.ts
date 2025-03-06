729794c1af3c0a29fded9ed55a0e9f387bda8528
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-03-06 12:16:43 +0000

    Change invariant helper a bit: now it takes class constructor as third parameter and just message as a second one. It allows avoid Error instantiations when they aren't necessary

--- src/lib/invariant.ts
+++ src/lib/invariant.ts
@@ -1,5 +1,12 @@
-export function invariant(condition: unknown, message: string | Error): asserts condition {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Newable<T> = new (...args: any[]) => T;
+
+export function invariant(
+  condition: unknown,
+  message: string,
+  ErrorClass: Newable<Error> = Error,
+): asserts condition {
   if (!condition) {
-    throw message instanceof Error ? message : new Error(message);
+    throw new ErrorClass(message);
   }
 }
--- src/lib/og-image/parseTokenOptions.ts
+++ src/lib/og-image/parseTokenOptions.ts
@@ -10,8 +10,8 @@ export async function parseTokenOptions<Schema extends z.AnyZodObject>(
 ): Promise<z.infer<Schema>> {
   const secret = getOgImageSecret();
 
-  invariant(signedJWTToken, new OGImageError("Token isn't specified"));
-  invariant(secret, new OGImageError("Secret isn't specified"));
+  invariant(signedJWTToken, "Token isn't specified", OGImageError);
+  invariant(secret, "Secret isn't specified", OGImageError);
 
   const verifiedJWT = await jwtVerify(signedJWTToken, secret);
   return schema.parse(verifiedJWT.payload);
