2785d86adfab0a05454311bd7e41f2481d88342c
Joe <98909677+moonclavedev@users.noreply.github.com>
2025-03-12 13:57:31 -0700

    [expressive code] Enable collapsible sections

---
+++ ec.config.mjs
@@ -0,0 +1,11 @@
+/* eslint-disable import-x/no-anonymous-default-export */
+import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
+
+/** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
+export default {
+  plugins: [pluginCollapsibleSections()],
+  defaultProps: {
+    // Change the default style of collapsible sections
+    collapseStyle: "collapsible-auto",
+  },
+};
