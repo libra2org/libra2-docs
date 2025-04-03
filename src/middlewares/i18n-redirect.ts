5001482d2f418041d9f4c1858997ca518f74a5e7
Pavel Grinchenko <psd-coder@evilmartians.com>
2025-04-03 19:47:43 +0100

    Don't redirect crawlers based on their accept-language header

--- src/middlewares/i18n-redirect.ts
+++ src/middlewares/i18n-redirect.ts
@@ -4,10 +4,78 @@ const LANGUAGE_CODES = SUPPORTED_LANGUAGES.map((lang) => lang.code);
 const DEFAULT_LANG = "en";
 const NON_DEFAULT_LANGS = LANGUAGE_CODES.filter((code) => code !== DEFAULT_LANG);
 
+const MOST_COMMON_CRAWLERS = [
+  "Googlebot\\/",
+  "Googlebot-Mobile",
+  "Googlebot-Image",
+  "Googlebot-News",
+  "Googlebot-Video",
+  "AdsBot-Google([^-]|$)",
+  "AdsBot-Google-Mobile",
+  "Feedfetcher-Google",
+  "Mediapartners-Google",
+  "Mediapartners \\(Googlebot\\)",
+  "APIs-Google",
+  "Google-InspectionTool",
+  "Storebot-Google",
+  "GoogleOther",
+  "bingbot",
+  "LinkedInBot",
+  "yandex\\.com\\/bots",
+  "Baiduspider",
+  "ezooms",
+  "heritrix",
+  "Ahrefs(Bot|SiteAudit)",
+  "IndeedBot",
+  "ZoominfoBot",
+  "SeznamBot",
+  "facebookexternalhit",
+  "Twitterbot",
+  "BUbiNG",
+  "Applebot",
+  "Slack-ImgProxy",
+  "SkypeUriPreview",
+  "Slackbot",
+  "redditbot",
+  "Google-Adwords-Instant",
+  "WhatsApp",
+  "pinterest\\.com\\/bot",
+  "BingPreview\\/",
+  "Yahoo Link Preview",
+  "Discordbot",
+  "TelegramBot",
+  "DuckDuckGo-Favicons-Bot",
+  "^Apache-HttpClient",
+  "AppEngine-Google",
+  "Google Web Preview",
+  "Baidu-YunGuanCe",
+  "FlipboardProxy",
+  "google-xrawler",
+  "Amazon CloudFront",
+  "Google-Structured-Data-Testing-Tool",
+  "ZoomBot",
+  "W3C_Validator",
+].map((crawler) => new RegExp(crawler, "i"));
+
+function isCrawler(userAgent: string | null): boolean {
+  if (!userAgent) {
+    return true; // Default to true if user-agent is not provided
+  }
+
+  return MOST_COMMON_CRAWLERS.some((regexp) => regexp.test(userAgent));
+}
+
 // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
 export default function middleware(request: Request): Response | void {
   const url = new URL(request.url);
   const pathname = url.pathname;
+
+  // Check if the request is from a crawler
+  const userAgent = request.headers.get("user-agent");
+
+  if (isCrawler(userAgent)) {
+    return undefined; // Do not redirect crawlers
+  }
 
   // Check for cookie
   const cookies = request.headers.get("cookie") ?? "";
